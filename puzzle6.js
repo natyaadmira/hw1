8f60dceaffe0c14445bf90b9e9df4d910e4f8343
natyaadmira <35670024+natyaadmira@users.noreply.github.com>
2018-01-22 22:50:29 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,21 @@
+function LeftorRight {
+	if (GetColor == "red"){
+	left 
+	} else {
+	right
+	}
+}
+right
+if (GetColor == "red"){
+	up
+	up
+	up
+	LeftorRight
+	up
+} else {
+	down
+	down
+	down
+	LeftorRight
+	down
+	}
