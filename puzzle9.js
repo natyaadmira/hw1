29831eda0167167f06cc05e389f94c608c2b872d
natyaadmira <35670024+natyaadmira@users.noreply.github.com>
2018-01-22 22:52:14 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,20 @@
+var count = 0
+while(count < 4){
+	down
+	count = count + 1
+}
+count = 0
+while (count < 9){
+	right
+	count = count + 1
+}
+count = 0
+while (count < 4){
+	up
+	count = count + 1
+}
+count = 0
+while (count < 9){
+	left
+	count = count + 1
+}
