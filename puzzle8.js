7af7809d22e6faf1b34bbb698348e48219709980
natyaadmira <35670024+natyaadmira@users.noreply.github.com>
2018-01-22 22:51:53 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,27 @@
+function KeepGoingDownBlank {
+	var count = 0
+	while (count < 7){
+		down
+	}
+}
+function KeepGoingUpBlank {
+	var count = 0
+	while (count < 7){
+		up
+	}
+}
+down
+KeepGoingDownBlank
+right
+right
+KeepGoingUpBlank
+right
+right
+KeepGoingDownBlank
+right
+right
+KeepGoingUpBlank
+right
+right
+KeepGoingDownBlank
+right
